10067ca10012f83b12b3ca4585de0fb139e3c386
Chaiwa Berian <chaiwa.berian@gmail.com>
2022-06-03 05:11:11 +0200

    MIS-190 #comment PrismaSelect

--- server/src/api/queries/DistrictQueries.ts
+++ server/src/api/queries/DistrictQueries.ts
@@ -1,15 +1,27 @@
 import { GraphQLContext } from "../..";
+import { PrismaSelect } from "@paljs/plugins";
+import { GraphQLResolveInfo } from "graphql";
 
-function getDistrictById(id: string, context: GraphQLContext) {
+function getDistrictById(
+  id: string,
+  context: GraphQLContext,
+  info: GraphQLResolveInfo
+) {
+  const select = new PrismaSelect(info).value;
   return context.prisma.district.findUnique({
     where: {
       id,
     },
+    ...select,
   });
 }
 
-function getProvinceByDistrictId(id: string, context: GraphQLContext) {
-  return getDistrictById(id, context).province();
+function getProvinceByDistrictId(
+  id: string,
+  context: GraphQLContext,
+  info: GraphQLResolveInfo
+) {
+  return getDistrictById(id, context, info).province();
 }
 // TODO: Refactor this
 function getCatchmentDistrictsByOrganisationId(
--- server/src/api/resolvers/relations/DistrictResolvers.ts
+++ server/src/api/resolvers/relations/DistrictResolvers.ts
@@ -2,5 +2,6 @@ import { DistrictResolvers } from "../../../libs/resolvers-types";
 import { getProvinceByDistrictId } from "../../queries";
 
 export const districtResolvers: DistrictResolvers = {
-  province: ({ id }, _args, context) => getProvinceByDistrictId(id, context),
+  province: ({ id }, _args, context, info) =>
+    getProvinceByDistrictId(id, context, info),
 };
