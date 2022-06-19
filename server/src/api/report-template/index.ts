5b1ce1d32c7de8555e6766347c041f51423ce74b
Chaiwa Berian <chaiwa.berian@gmail.com>
2022-06-19 16:53:19 +0200

    MIS-161 #comment Add ReportTemplates

--- server/src/api/report-template/index.ts
+++ server/src/api/report-template/index.ts
@@ -1,12 +1,12 @@
 import { gql } from "apollo-server-express";
 import { Resolvers } from "../../libs/resolvers-types";
-// import {
-//   createReportTemplate,
-//   deleteReportTemplate,
-//   getReportTemplate,
-//   getReportTemplates,
-//   updateReportTemplate,
-// } from "../queries";
+import {
+  createReportTemplate,
+  deleteReportTemplate,
+  getReportTemplate,
+  getReportTemplates,
+  updateReportTemplate,
+} from "../queries";
 
 const typeDefs = gql`
   type ReportTemplate {
@@ -83,16 +83,20 @@ const typeDefs = gql`
 
 const resolvers: Resolvers = {
   Query: {
-    // report_template: (_, args, context) => getReportTemplate(args, context),
-    // report_templates: (_, _args, context) => getReportTemplates(context),
+    report_template: (_, args, context) => getReportTemplate(args, context),
+    report_templates: (_, _args, context) => getReportTemplates(context),
   },
   Mutation: {
-    // createReportTemplate: (_, args, context) =>
-    //   createReportTemplate(args, context),
-    // updateReportTemplate: (_, args, context) =>
-    //   updateReportTemplate(args, context),
-    // deleteReportTemplate: (_, args, context) =>
-    //   deleteReportTemplate(args, context),
+    createReportTemplate: (_, args, context) =>
+      createReportTemplate(args, context),
+    updateReportTemplate: (_, args, context) =>
+      updateReportTemplate(args, context),
+    deleteReportTemplate: (_, args, context) =>
+      deleteReportTemplate(args, context),
+  },
+  ReportTemplate: {
+    // indicators: (parent,_args, context)=> getIndicators,
+    // organisation_report_templates: (parent, _args, context)=>getOrganisationReportTemplates()
   },
 };
 
